6bfdc3f2d036e8c5b9f7573de154b5ed6a051a77
praveen.sharma <praveen.sharma@locationlabs.com>
2014-03-16 01:20:10 +0530

    new xcode project for infinite scrolling ..

---
+++ JHInfiniteScrollView/JHInfiniteScrollView/JHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JHAppDelegate.h
+//  JHInfiniteScrollView
+//
+//  Created by Praveen Sharma on 16/03/14.
+//  Copyright (c) 2014 Jhaliya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
